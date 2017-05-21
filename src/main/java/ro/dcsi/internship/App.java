54abdd058713d43733e9b9fac862e84785b9e000
patrascujustinlucian <patrascu.justin@yahoo.com>
2017-05-21 17:05:02 +0300

    Add files via upload

--- src/main/java/ro/dcsi/internship/App.java
+++ src/main/java/ro/dcsi/internship/App.java
@@ -1,13 +1,28 @@
 package ro.dcsi.internship;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App {
+	
+    public static void main( String[] args ) throws IOException {
+    	FileReader fr = new FileReader("D:/workspace/ship2/users100.csv");
+    	BufferedReader br = new BufferedReader(fr);
+    	FileWriter f0 = new FileWriter("D:/workspace/ship2/users100out.csv");
+    	String s;
+    	
+    	try{ 
+    	while((s = br.readLine()) != null) {
+    	f0.append(s+",");
+    	}
+    	}catch (IOException e) {
+            e.printStackTrace();
+            }
+    	fr.close();
+    	f0.close();
+    	
     }
 }
