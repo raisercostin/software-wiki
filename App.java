326ea57ad371d2556f39f26344e098a081cd8e10
radu099117 <radumoreni@icloud.com>
2017-05-22 02:25:18 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package ro.dcsi.internship;
+
+/**
+ * Hello world!
+ *
+ */
+//facem nou branch user/numele nostru si punem proiectu(2 fisiere test, main, pom).
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! by Radu" );
+    }
+}
---
+++ ReadFileTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.dcsi.internship;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ *
+ * @author soiganradu
+ */
+public class ReadFileTest extends TestCase {
+    
+    public ReadFileTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    @Test
+    public void test(){
+        fail("Not yet implemented");
+    }
+
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+}
