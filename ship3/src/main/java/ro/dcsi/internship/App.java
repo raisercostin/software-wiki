8b9b57dbcf08cdd96f331b832fa29e707bca35cb
Teodorescu Stefan Eduard <teodorescu.stefan.eduard@gmail.com>
2017-05-21 14:47:28 +0300

    First commit

---
+++ ship3/src/main/java/ro/dcsi/internship/App.java
@@ -0,0 +1,11 @@
+package ro.dcsi.internship;
+
+// Tema de trimis codul pe git pe un branck propriu in spatiul comun la team/user/stefan
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! by stefan" );
+    }
+}
