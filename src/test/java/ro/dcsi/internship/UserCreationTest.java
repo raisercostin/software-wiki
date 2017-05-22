a2f456b76f33db60fc47d29f052ddfcac84ed8be
raisercostin <raisercostin@gmail.com>
2017-05-22 11:57:40 +0300

    refactored

--- src/test/java/ro/dcsi/internship/UserCreationTest.java
+++ src/test/java/ro/dcsi/internship/UserCreationTest.java
@@ -2,19 +2,23 @@ package ro.dcsi.internship;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class UserCreationTest {
-
 	@Test
-	public void test() {
-		if(App.getExportedusersfile().length()>0){
-			assertTrue(true);
-		}
-		else if(App.getImportedusersfile().length()>0)
-		{
-			fail("Users were not exported.");
-		}
+	public void testImportedFileExists() throws IOException {
+		App.main(null);
+		assertTrue("Numarul de useri nu este pozitiv: "+App.getExportedusersfile().length(),App.getImportedusersfile().length() > 0);
+		assertEquals(11998,App.getImportedusersfile().length());
 	}
-
+	@Test
+	public void testExportedFile() throws IOException {
+		App.main(null);
+		assertTrue("Numarul de useri nu este pozitiv: "+App.getExportedusersfile().length(),App.getExportedusersfile().length() > 0);
+		assertEquals(12991,App.getExportedusersfile().length());
+		assertEquals(App.getImportedusersfile().length()
+ 				,App.getExportedusersfile().length());
+	}
 }
