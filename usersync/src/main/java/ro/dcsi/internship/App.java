60d5fb2f78c064ec1ff77fa577b0e509d3501be1
raisercostin <raisercostin@gmail.com>
2017-05-22 22:15:20 +0300

    import users

---
+++ usersync/src/main/java/ro/dcsi/internship/App.java
@@ -0,0 +1,13 @@
+package ro.dcsi.internship;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! by costin" );
+    }
+}
