fb2815e8094f0457c5e95c2b950f7fa1bfe9bd21
raisercostin <raisercostin@gmail.com>
2017-05-22 22:12:48 +0300

    import app

---
+++ usersync/src/test/java/ro/dcsi/internship/FileReadTest.java
@@ -0,0 +1,14 @@
+package ro.dcsi.internship;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileReadTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
