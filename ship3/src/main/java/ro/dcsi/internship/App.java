b4cee6e054cb94d62d9a2f76f6ec67cb87863365
Cosmin Rezmerita <cosmin.rezmerita@gmail.com>
2017-05-23 22:56:49 +0300

    some modif

--- ship3/src/main/java/ro/dcsi/internship/App.java
+++ ship3/src/main/java/ro/dcsi/internship/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World2" );
+        System.out.println( "Hello World3" );
     }
 }
