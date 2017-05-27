2d8ec16c123c45ae2084bc65a606dda458694885
1Mihail <gabriel714@yahoo.com>
2017-05-27 23:27:20 +0300

    Improved tests

--- src/test/java/ro/dcsi/internship/UserCreationTest.java
+++ src/test/java/ro/dcsi/internship/UserCreationTest.java
@@ -12,19 +12,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class UserCreationTest {
-
-	@Test
-	public void testImportedFileExists() throws IOException {
-		assertTrue(App.getImportedusersfile().exists());
-	}
-
-	@Test
-	public void testExportedFile() throws IOException {
-		assertTrue(App.getExportedusersfile().exists());
-	}
-
 	@Test
 	public void successfulCreation() throws IOException {
+		App.export(App.getImportedusersfile().toString(),App.getExportedusersfile().toString());
 		LineNumberReader exportedEntries = new LineNumberReader(new FileReader(App.getExportedusersfile()));
 		LineNumberReader importedEntries = new LineNumberReader(new FileReader(App.getImportedusersfile()));
 		exportedEntries.skip(Long.MAX_VALUE);
@@ -32,5 +22,23 @@ public class UserCreationTest {
 		Assert.assertEquals(exportedEntries.getLineNumber(), importedEntries.getLineNumber());
 		exportedEntries.close();
 		importedEntries.close();
+	}
+	
+	@Test
+	public void testImportedFileExists() throws IOException {
+		assertTrue("Numarul de useri nu este pozitiv: " + App.getExportedusersfile().length(),
+				App.getImportedusersfile().length() > 0);
+		assertTrue(App.getImportedusersfile().exists());
+	}
+
+	@Test
+	public void testExportedFile() throws IOException {
+		App.export(App.getImportedusersfile().toString(), "target/exportedUsers1.csv");
+		assertEquals("exportedUsers1.csv", App.getExportedusersfile().getName());
+		
+		App.export(App.getImportedusersfile().toString(), "target/exportedUsers2.csv");
+		assertEquals("exportedUsers2.csv", App.getExportedusersfile().getName());
+		
+		assertTrue(App.getExportedusersfile().exists());
 	}
 }
