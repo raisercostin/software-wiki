4d4761b272eda36430b471af994bc1eee4700a80
raisercostin <raisercostin@gmail.com>
2017-05-29 01:13:54 +0300

    duplicated

--- src/main/java/ro/dcsi/internship/UserManager.java
+++ src/main/java/ro/dcsi/internship/UserManager.java
@@ -17,49 +17,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.List;
 public class UserManager {
-	public static void main(String[] args) throws IOException {
-		String readCsv = "c:\\Users\\Andrei\\maven workspace\\ship1\\src\\main\\resources\\HundredUsers.csv";
-		String writeCsv = "c:\\Users\\Andrei\\maven workspace\\ship1\\src\\test\\resources\\WriteHundredUsers.csv";
-
-		BufferedReader br = null;
-		FileWriter fr = null;
-		String line = "";
-		String wordSeparator = ",";
-		try {
-			br = new BufferedReader(new FileReader(readCsv));
-			fr = new FileWriter(writeCsv);
-			while ((line = br.readLine()) != null) {
-				String[] usersFromCsv = line.split(wordSeparator);
-				for (String user : usersFromCsv) {
-						System.out.println(user);
-						fr.append(user + ",").append("\n");
-					}
-
-					
-				}
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			try {
-				fr.flush();
-				fr.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-
-			}
-		}
-	}
-
     public static void main2( String[] args )
     {
         List<User> users = new CsvExporter().readUsers("/home/madalin/Workspace/Eclipse/WORK/UserSyncApp/ship1/src/main/resources/sample1.csv");
