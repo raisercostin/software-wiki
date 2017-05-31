834e4744c8bd1cde0d5d120923c55a60507e3050
patrascujustinlucian <patrascu.justin@yahoo.com>
2017-05-31 11:18:07 +0300

    Update PutOnServer.java

--- src/main/java/ro/dcsi/internship/PutOnServer.java
+++ src/main/java/ro/dcsi/internship/PutOnServer.java
@@ -1,4 +1,5 @@
 
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.io.*;
@@ -6,21 +7,29 @@ import org.json.simple.JSONObject;
 
 public class PutOnServer implements UserDao {
 	
-	URL url = new URL (http://localhost:8080);
+	static URL url = new URL (//localhost:8080);
 	URLConnection connection = url.openConnection();
-	HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+	static HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
 
 	connection.setDoOutput(true);
-    OutputStreamWriter writer=new OutputStreamWriter(conn.getOutputStream());
-    JSONObject json=null;
+	httpCon.setRequestMethod("PUT");
+	
+    OutputStreamWriter writer=new OutputStreamWriter(connection.getOutputStream());
+    static JSONObject json=null;
 
-    public static putDetails(int id, String mail, String userName, String sn, String givenName){
+    public static void putDetails(int id, String mail, String userName, String sn, String givenName){
+    	httpCon.setRequestProperty("Content-Type", "application/json");
+    	httpCon.setRequestProperty("Accept", "application/json");
+    	httpCon.setRequestProperty("If-None-Match", "*'");
+     	httpCon.setRequestProperty("X-OpenIDM-Username", "openidm-admin");
+     	httpCon.setRequestProperty("X-OpenIDM-Username", "openidm-admin");
+     	httpCon.setRequestProperty("X-Requested-With", "Swagger-UI");
     	json=new JSONObject();
     	json.put("id", 2023 );
-    	json.put("mail", lukas4@gmail.com );
-    	json.put("userName", lukas,  );
-    	json.put("sn", sn210,  );
-    	json.put("givenName", lukasss,  );
+    	json.put("mail", "lukas4@gmail.com" );
+    	json.put("userName", "lukas");
+    	json.put("sn", "sn210");
+    	json.put("givenName", "lukasss"  );
     }
 
 
