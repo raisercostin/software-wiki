d716f35720373c3f11f23a65c3819d1c67447f8b
BuzzDeejayLord <lord_ando10@yahoo.com>
2017-06-19 15:45:41 +0300

    first simple maven project

---
+++ src/main/java/ro/dcsi/internship/User.java
@@ -0,0 +1,7 @@
+package ro.dcsi.internship;
+
+public class User {
+
+	public String name;
+
+}
---
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -0,0 +1,12 @@
+package ro.dcsi.internship;
+
+import java.util.List;
+
+public class UserSync {
+
+	public static List<User> readUsers(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
