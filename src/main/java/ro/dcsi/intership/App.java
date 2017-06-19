727788c94da27468bdce0a742ad603a6f88c2c78
raisercostin <raisercostin@gmail.com>
2017-06-19 15:20:05 +0300

    first simple maven project

---
+++ src/main/java/ro/dcsi/intership/App.java
@@ -0,0 +1,13 @@
+package ro.dcsi.intership;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
