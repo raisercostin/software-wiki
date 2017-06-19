654231f3157661a952a217d05ffeb6284de9604d
BuzzDeejayLord <lord_ando10@yahoo.com>
2017-06-19 20:17:21 +0300

    CSV File Reader

---
+++ CSV_File_Reader/src/CSVReader.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+
+    public static void main(String[] args) {
+
+        String csvFile = "SampleCSVFile.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                
+                String[] info = line.split(cvsSplitBy);
+
+                System.out.println("Data [code= " + info[4] + " , name=" + info[2] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
