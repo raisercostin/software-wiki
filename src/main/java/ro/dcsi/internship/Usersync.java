88c85228356678faf9d1210d8028c17653cd12a4
Catalin Ghenea <catalin.ghenea@gmail.com>
2017-06-20 09:35:42 +0300

    small define in main class

--- src/main/java/ro/dcsi/internship/Usersync.java
+++ src/main/java/ro/dcsi/internship/Usersync.java
@@ -4,12 +4,14 @@ package ro.dcsi.internship;
  * Created by Catalin on 6/19/2017.
  */
 public class Usersync {
+    public static final int bulkDefine = 100;
+
     public Usersync() {
     }
 
     public void readUsers(String Filename){
         Translator translator = new TranslatorCSV();
-        UserManager database = new UserManager(100);
+        UserManager database = new UserManager(Usersync.bulkDefine);
 
         translator.setInputFile(Filename);
         database.setUserFile(translator);
