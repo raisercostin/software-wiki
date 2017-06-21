3ef73836ff97e10a4decddb2a54b3dbb8b089ef3
Laur Tor <Laur Tor>
2017-06-21 14:51:11 +0300

    up 2 date

---
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -0,0 +1,16 @@
+package ro.dcsi.internship;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserSync {
+
+	public static List<User> readUsers(String fileName) {
+		throw new RuntimeException("Not implemented yet!!!!");
+	}
+
+	public static Iterable<User> readUsersFromHugeFile(String fileName) {
+		throw new RuntimeException("Not implemented yet!!!!");
+	}
+
+}
---
+++ src/test/java/ro/dcsi/internship/UserSyncTest.java
@@ -0,0 +1,32 @@
+package ro.dcsi.internship;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+
+public class UserSyncTest {
+
+	@Test
+	public void test() {
+		List<User> users = UserSync.readUsers("users1.csv");
+		assertNotNull(users);
+		assertEquals(5,users.size());
+		assertEquals("ion",users.get(0).name);
+	}
+	@Test
+	public void testHugeFile() {
+		Iterable<User> users = UserSync.readUsersFromHugeFile("users1.csv");
+		User last = lastFrom(users);
+		assertEquals("john",last.name);
+	}
+	private User lastFrom(Iterable<User> users) {
+		User last = null;
+		for (User user : users) {
+			last = user;
+		}
+		return last;
+	}
+}
