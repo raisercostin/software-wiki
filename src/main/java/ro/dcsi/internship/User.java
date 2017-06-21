160a2fb97b4bb0bc05dda5058c9d466de1d5d93b
Laur Tor <Laur Tor>
2017-06-21 15:15:21 +0300

    Implemented readfromcsv

--- src/main/java/ro/dcsi/internship/User.java
+++ src/main/java/ro/dcsi/internship/User.java
@@ -2,4 +2,8 @@ package ro.dcsi.internship;
 
 public class User {
 	public String name;
+
+	public User(String name) {
+		this.name = name;
+	}
 }
--- src/main/java/ro/dcsi/internship/UserSync.java
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -1,16 +1,61 @@
 package ro.dcsi.internship;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.opencsv.CSVReader;
 
 public class UserSync {
 
 	public static List<User> readUsers(String fileName) {
-		throw new RuntimeException("Not implemented yet!!!!");
+		CSVReader csvReader = null;
+		List<User> empList = new ArrayList<User>();
+		try {
+			/**
+			 * Reading the CSV File Delimiter is comma Start reading from line 1
+			 */
+			csvReader = new CSVReader(new FileReader("users1.csv"), ',', '"', 1);
+			// employeeDetails stores the values current line
+			String[] employeeDetails = null;
+			// Create List for holding Employee objects
+
+			while ((employeeDetails = csvReader.readNext()) != null) {
+				// Save the employee details in Employee object
+				User emp = new User(employeeDetails[0]);
+
+				empList.add(emp);
+			}
+
+		} catch (Exception ee) {
+			ee.printStackTrace();
+		}
+		return empList;
 	}
 
 	public static Iterable<User> readUsersFromHugeFile(String fileName) {
-		throw new RuntimeException("Not implemented yet!!!!");
+		CSVReader csvReader = null;
+		List<User> empList = new ArrayList<User>();
+		try {
+			/**
+			 * Reading the CSV File Delimiter is comma Start reading from line 1
+			 */
+			csvReader = new CSVReader(new FileReader("users1.csv"), ',', '"', 1);
+			// employeeDetails stores the values current line
+			String[] employeeDetails = null;
+			// Create List for holding Employee objects
+
+			while ((employeeDetails = csvReader.readNext()) != null) {
+				// Save the employee details in Employee object
+				User emp = new User(employeeDetails[0]);
+
+				empList.add(emp);
+			}
+
+		} catch (Exception ee) {
+			ee.printStackTrace();
+		}
+		return empList;
 	}
 
 }
