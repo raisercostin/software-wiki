4cdd5a2c479222e510c70a81ca5d6ff19764c1e4
knston99 <bogdan.petras96@yahoo.com>
2017-06-21 14:34:49 +0300

    readUser method

--- usersync/src/main/java/ro/dcsi/internship/User.java
+++ usersync/src/main/java/ro/dcsi/internship/User.java
@@ -1,5 +1,21 @@
 package ro.dcsi.internship;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 public class User {
+
+	public String name;
+	public int age;
+	public double salary;
+	public String details;
+	
+	@Override
+	public String toString()
+	{
+		return name+" "+age+" "+salary+" "+details;
+	}
 
 }
--- usersync/src/test/java/ro/dcsi/internship/UserSyncTest.java
+++ usersync/src/test/java/ro/dcsi/internship/UserSyncTest.java
@@ -7,14 +7,34 @@ import java.util.List;
 import org.junit.Test;
 
 
-public class UserSyncTest {
+public class UserSyncTest 
+{
 
 	@Test
-	public void test() {
+	public void test() 
+	{
 
 		List<User> users = UserSync.readUser("users1.csv");
 		assertNotNull(users);
-		assertEquals(5,users.size());
+		assertEquals(3,users.size());
+		assertEquals("ion andrei",users.get(0).name);
+	}
+	
+	@Test
+	public void testHugeFile() 
+	{
+		Iterable<User> users = UserSync.readUsersFromHugeFile("users1.csv");
+		User last = lastFrom(users);
+		assertEquals("john",last.name);
+	}
+	private User lastFrom(Iterable<User> users) 
+	{
+		User last = null;
+		for (User user : users) 
+		{
+			last = user;
+		}
+		return last;
 	}
 
 }
