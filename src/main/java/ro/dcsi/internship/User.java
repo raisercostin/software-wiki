7ea87111d5c10d366d6d723f72125a701ce89a50
BuzzDeejayLord <lord_ando10@yahoo.com>
2017-06-21 11:30:54 +0300

    Code to read CSV File

--- src/main/java/ro/dcsi/internship/User.java
+++ src/main/java/ro/dcsi/internship/User.java
@@ -3,5 +3,12 @@ package ro.dcsi.internship;
 public class User {
 
 	public String name;
+	public User(){}
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
 
 }
--- src/main/java/ro/dcsi/internship/UserSync.java
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -1,12 +1,50 @@
 package ro.dcsi.internship;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserSync {
 
 	public static List<User> readUsers(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+        String csvFile = "SampleCSVFile.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        List<User> users= new ArrayList<User>();
+        User temp=new User();
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                
+                String[] info = line.split(cvsSplitBy);
+                temp=new User(info[1]);
+                users.add(temp);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return users;
+    }
 
 }
+
+
