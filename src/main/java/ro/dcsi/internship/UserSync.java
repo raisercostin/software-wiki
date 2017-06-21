6a0a126904569c314c7ce47257244bf3888dcabc
raisercostin <raisercostin@gmail.com>
2017-06-21 18:27:42 +0300

    bugfix

--- src/main/java/ro/dcsi/internship/UserSync.java
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -6,63 +6,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class UserSync 
-{
+public class UserSync {
 
-	public static List<User> readUser(String title) 
-	{
-		// TODO Auto-generated method stub
-		//throw new RuntimeException("Not implemented yet!");
-		//return null;
-		Scanner sc = null;
-		List<User> lu = new ArrayList<User>();
-		try 
-		{
-			 sc = new Scanner(new File(title)).useDelimiter(",[\\n\\r]{0,2}");
-			 
-			 while(sc.hasNextLine())
-		     {
-		     	String line = sc.nextLine();
-		       	String w[] = line.split("[,]");
-		       	User curent_user = new User();
-		       	
-		        curent_user.name = w[0]+" "+w[1];
-		        curent_user.age = Integer.parseInt(w[2]);
-		        curent_user.salary = Double.parseDouble(w[3]);
-		        for(int i = 4; i < w.length; i++)
-		        	if(curent_user.details != null)
-		        		curent_user.details = curent_user.details + w[i]+" ";
-		        	else
-		        		curent_user.details = w[i] + " ";
-		        
-		        lu.add(curent_user);
-		            
-		     }
-			 
-		} 
-		catch (FileNotFoundException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static List<User> readUser(String title) {
+		try (Scanner sc = new Scanner(new File(title)).useDelimiter(",[\\n\\r]{0,2}");) {
+			List<User> lu = new ArrayList<User>();
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String w[] = line.split("[,]");
+				User curent_user = new User();
+				curent_user.name = w[0] + " " + w[1];
+				curent_user.age = Integer.parseInt(w[2]);
+				curent_user.salary = Double.parseDouble(w[3]);
+				for (int i = 4; i < w.length; i++)
+					if (curent_user.details != null)
+						curent_user.details = curent_user.details + w[i] + " ";
+					else
+						curent_user.details = w[i] + " ";
+				lu.add(curent_user);
+			}
+			return lu;
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
 		}
-		finally
-		{
-			sc.close();
-			
-			for(User user:lu) //auto-test
-				System.out.println(user);
-			
-		}
-		return lu;
-		
 	}
 
-	public static Iterable<User> readUsersFromHugeFile(String title) 
-	{
-		//throw new RuntimeException("Not implemented yet!!!!");
+	public static Iterable<User> readUsersFromHugeFile(String title) {
+		// throw new RuntimeException("Not implemented yet!!!!");
 		Scanner sc = null;
 		Iterable<User> lu = new ArrayList<User>();
-		
+
 		return lu;
 	}
 
--- src/test/java/ro/dcsi/internship/UserSyncTest.java
+++ src/test/java/ro/dcsi/internship/UserSyncTest.java
@@ -6,31 +6,41 @@ import java.util.List;
 
 import org.junit.Test;
 
-
-public class UserSyncTest 
-{
-
+public class UserSyncTest {
 	@Test
-	public void test() 
-	{
+	public void test() {
 		List<User> users = UserSync.readUser("src/test/resources/users1.csv");
 		assertNotNull(users);
-		assertEquals(3,users.size());
-		assertEquals("ion andrei",users.get(0).name);
+		assertEquals(3, users.size());
+		assertEquals("ion andrei", users.get(0).name);
 	}
-	
+
 	@Test
-	public void testHugeFile() 
-	{
+	public void testFileDoesntExist() {
+		try {
+			List<User> users = UserSync.readUser("src/test/resources/users1-nonexisting.csv");
+			fail("should not return normally from readUser");
+		} catch (RuntimeException e) {
+			assertNotNull(e);
+			assertTrue(e.getMessage().contains("nonexisting.csv"));
+		}
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testFileDoesntExistSimpleCheck() {
+		UserSync.readUser("src/test/resources/users1-nonexisting.csv");
+	}
+
+	@Test
+	public void testHugeFile() {
 		Iterable<User> users = UserSync.readUsersFromHugeFile("users1.csv");
 		User last = lastFrom(users);
-		assertEquals("john",last.name);
+		assertEquals("john", last.name);
 	}
-	private User lastFrom(Iterable<User> users) 
-	{
+
+	private User lastFrom(Iterable<User> users) {
 		User last = null;
-		for (User user : users) 
-		{
+		for (User user : users) {
 			last = user;
 		}
 		return last;
