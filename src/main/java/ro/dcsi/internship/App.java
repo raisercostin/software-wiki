07b0ce9efd083d82baf01e3035197b098694b10a
DJakarta <djbogdy3x@yahoo.com>
2017-06-26 13:05:31 +0300

    Running base

--- src/main/java/ro/dcsi/internship/App.java
+++ src/main/java/ro/dcsi/internship/App.java
@@ -1,13 +1,7 @@
 package ro.dcsi.internship;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
