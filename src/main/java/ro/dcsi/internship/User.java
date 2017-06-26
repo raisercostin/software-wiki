8f66a7cf357542b778cfc98c40ebfb324627caae
raisercostin <raisercostin@gmail.com>
2017-06-26 17:47:06 +0300

    reformat

--- src/main/java/ro/dcsi/internship/User.java
+++ src/main/java/ro/dcsi/internship/User.java
@@ -1,23 +1,23 @@
 package ro.dcsi.internship;
 
 public class User {
-	public final String surname;
-	public final String firstname;
-	public final String age;
-	public final String nationality;
-	public final String country;
+    public final String surname;
+    public final String firstname;
+    public final String age;
+    public final String nationality;
+    public final String country;
 
-	public User(String surname, String firstname, String age, String nationality, String country) {
-		this.surname = surname;
-		this.firstname = firstname;
-		this.age = age;
-		this.nationality = nationality;
-		this.country = country;
-	}
+    public User(String surname, String firstname, String age, String nationality, String country) {
+        this.surname = surname;
+        this.firstname = firstname;
+        this.age = age;
+        this.nationality = nationality;
+        this.country = country;
+    }
 
-	@Override
-	public String toString() {
-		return "User [surname=" + surname + ", firstname=" + firstname + ", age=" + age + ", nationality=" + nationality
-				+ ", country=" + country + "]";
-	}
+    @Override
+    public String toString() {
+        return "User [surname=" + surname + ", firstname=" + firstname + ", age=" + age + ", nationality=" + nationality
+                + ", country=" + country + "]";
+    }
 }
