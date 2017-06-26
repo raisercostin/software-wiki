93a1b0554092b7e7813ee60b26cda463ef472757
BuzzDeejayLord <lord_ando10@yahoo.com>
2017-06-26 14:01:41 +0300

    Add write to csv tool

--- src/main/java/ro/dcsi/internship/UserSync.java
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,4 +26,43 @@ public class UserSync {
 		}
 		return users;
 	}
+	
+    private static final char DEFAULT_SEPARATOR = ',';
+
+    public static void writeLine(Writer w, List<String> values) throws IOException {
+        writeLine(w, values, DEFAULT_SEPARATOR, ' ');
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators) throws IOException {
+        writeLine(w, values, separators, ' ');
+    }
+
+    private static String followCVSformat(String value) {
+        String result = value;
+        if (result.contains("\"")) {
+            result = result.replace("\"", "\"\"");
+        }
+        return result;
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators, char customQuote) throws IOException {
+        boolean first = true;
+        if (separators == ' ') {
+            separators = DEFAULT_SEPARATOR;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String value : values) {
+            if (!first) {
+                sb.append(separators);
+            }
+            if (customQuote == ' ') {
+                sb.append(followCVSformat(value));
+            } else {
+                sb.append(customQuote).append(followCVSformat(value)).append(customQuote);
+            }
+            first = false;
+        }
+        sb.append("\n");
+        w.append(sb.toString());
+    }
 }
--- src/test/java/ro/dcsi/internship/UserSyncTest.java
+++ src/test/java/ro/dcsi/internship/UserSyncTest.java
@@ -2,6 +2,9 @@ package ro.dcsi.internship;
 
 import static org.junit.Assert.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -14,8 +17,33 @@ public class UserSyncTest {
 		assertEquals(100, users.size());
 		assertEquals("Eldon Base for stackable storage shelf", users.get(0).name);
 	}
+	
 	@Test(expected=RuntimeException.class)
 	public void testFileNotFound() {
 		List<User> users = UserSync.readUsers("src/test/resources/SampleCSVFile-non-existing.csv");// "users.csv");
 	}
+	
+	@Test
+	public void writeText() throws IOException{
+
+        String csvFile = "src/test/resources/SampleWriteFile.csv";
+        FileWriter writer = new FileWriter(csvFile);
+
+        UserSync.writeLine(writer, Arrays.asList("a", "b", "c", "d"));
+
+        //custom separator + quote
+        UserSync.writeLine(writer, Arrays.asList("aaa", "bb,b", "cc,c"), ',', '"');
+
+        //custom separator + quote
+        UserSync.writeLine(writer, Arrays.asList("aaa", "bbb", "cc,c"), '|', '\'');
+
+        //double-quotes
+        UserSync.writeLine(writer, Arrays.asList("aaa", "bbb", "cc\"c"));
+
+
+        writer.flush();
+        writer.close();
+
+	}
+	
 }
