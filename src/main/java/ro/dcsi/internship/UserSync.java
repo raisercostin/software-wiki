e1c21882ea5e6925af1ce8921d60249bb00e1917
raisercostin <raisercostin@gmail.com>
2017-06-26 15:40:49 +0300

    bug fix and move test file

--- src/main/java/ro/dcsi/internship/UserSync.java
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -1,6 +1,8 @@
 package ro.dcsi.internship;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,46 +11,40 @@ import com.opencsv.CSVReader;
 public class UserSync {
 
 	public static List<User> readUsers(String fileName) {
-		CSVReader csvReader = null;
 		List<User> empList = new ArrayList<User>();
-		try {
+		try (CSVReader csvReader = new CSVReader(new FileReader(fileName), ',', '"', 1);) {
 			/**
 			 * Reading the CSV File Delimiter is comma Start reading from line 1
 			 */
-			csvReader = new CSVReader(new FileReader("users1.csv"), ',', '"', 1);
 			String[] employeeDetails = null;
-
 			while ((employeeDetails = csvReader.readNext()) != null) {
 				User emp = new User(employeeDetails[0], employeeDetails[1], employeeDetails[2], employeeDetails[3],
 						employeeDetails[4]);
 
 				empList.add(emp);
 			}
-
-		} catch (Exception ee) {
-			ee.printStackTrace();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 		return empList;
 	}
 
 	public static Iterable<User> readUsersFromHugeFile(String fileName) {
-		CSVReader csvReader = null;
 		List<User> empList = new ArrayList<User>();
-		try {
+		try(CSVReader csvReader = new CSVReader(new FileReader(fileName), ',', '"', 1)){
 			/**
 			 * Reading the CSV File Delimiter is comma Start reading from line 1
 			 */
-			csvReader = new CSVReader(new FileReader("users1.csv"), ',', '"', 1);
 			String[] employeeDetails = null;
-
 			while ((employeeDetails = csvReader.readNext()) != null) {
 				User emp = new User(employeeDetails[0], employeeDetails[1], employeeDetails[2], employeeDetails[3],
 						employeeDetails[4]);
 				empList.add(emp);
 			}
-
 		} catch (Exception ee) {
-			ee.printStackTrace();
+			throw new RuntimeException("While reading users from file " + fileName, ee);
 		}
 		return empList;
 	}
--- src/test/java/ro/dcsi/internship/UserSyncTest.java
+++ src/test/java/ro/dcsi/internship/UserSyncTest.java
@@ -11,14 +11,14 @@ public class UserSyncTest {
 
 	@Test
 	public void test() {
-		List<User> users = UserSync.readUsers("users1.csv");
+		List<User> users = UserSync.readUsers("src/test/resources/users1.csv");
 		assertNotNull(users);
 //		assertEquals(5,users.size());
 //		assertEquals("ion",users.get(0).surname);
 	}
 	@Test
 	public void testHugeFile() {
-		Iterable<User> users = UserSync.readUsersFromHugeFile("users1.csv");
+		Iterable<User> users = UserSync.readUsersFromHugeFile("src/test/resources/users1.csv");
 		User last = lastFrom(users);
 		assertEquals("john",last.surname);
 	}
