6c79f38fca9f77ca80657caa85cc0899c1c16db4
DJakarta <djbogdy3x@yahoo.com>
2017-07-05 11:01:42 +0300

    ...

--- src/main/java/ro/dcsi/internship/UserSync.java
+++ src/main/java/ro/dcsi/internship/UserSync.java
@@ -1,13 +1,7 @@
 package ro.dcsi.internship;
 
-import org.apache.commons.cli.*;
-
 public class UserSync {
   public void copyUsers(UserDao src, UserDao dest) {
     dest.write(src.read());
   }
-  /*public static void main(String args[]) {
-	Option.Builder builder = Option.builder();
-	Option sourceType = builder.
-  }*/
 }
