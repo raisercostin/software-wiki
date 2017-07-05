8abea517bb97416e9d45e1550222538b5a792c86
Catalin Ghenea <catalin.ghenea@gmail.com>
2017-07-05 10:07:04 +0300

    modify version of commons-cli

--- src/main/java/ro/dcsi/internship/UserSyncApp.java
+++ src/main/java/ro/dcsi/internship/UserSyncApp.java
@@ -1,8 +1,19 @@
 package ro.dcsi.internship;
+import org.apache.commons.cli.*;
 
 public class UserSyncApp {
   public static void main(String[] args) {
     //identify src, dest from parameters
+
+    //Create option groupe
+    OptionGroup options = new OptionGroup();
+
+
+    //Adding options
+    Option forgeRock = Option.b
+
+
+
     //create src/dest UserDao
     //call UserSync
     throw new RuntimeException("Not Implemented Yet!!!");
