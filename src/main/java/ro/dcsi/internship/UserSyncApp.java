9c583cc0f51d8051904447161db42004511a2b13
Catalin Ghenea <catalin.ghenea@gmail.com>
2017-07-05 11:35:47 +0300

    format style

--- src/main/java/ro/dcsi/internship/UserSyncApp.java
+++ src/main/java/ro/dcsi/internship/UserSyncApp.java
@@ -1,40 +1,40 @@
 package ro.dcsi.internship;
+
 import org.apache.commons.cli.*;
 
 public class UserSyncApp {
 
   //App name
-  private final static String appName="usersync";
+  private final static String appName = "usersync";
   private final static String usage = appName + " <src> <dest>";
 
   //Option strings
-  private final static String forgeRockShortOption="f";
-  private final static String forgeRockLongOption="forgerock";
-  private final static String csvShortOption="c";
-  private final static String csvLongOption="csv";
-
+  private final static String forgeRockShortOption = "f";
+  private final static String forgeRockLongOption = "forgerock";
+  private final static String csvShortOption = "c";
+  private final static String csvLongOption = "csv";
 
   //Print help
-  private static void printHelp(Options options){
+  private static void printHelp(Options options) {
     //Print Help
     HelpFormatter help = new HelpFormatter();
-    help.printHelp(usage,options);
+    help.printHelp(usage, options);
   }
 
   //Parse option for UserDao
-  private static UserDao parseDaoInfo(Option daoInfo){
+  private static UserDao parseDaoInfo(Option daoInfo) {
 
     //Parse CSV Dao
-    if(daoInfo.getOpt().equals(csvShortOption) || (daoInfo.hasLongOpt() && daoInfo.getLongOpt().equals(csvLongOption)))
+    if (daoInfo.getOpt().equals(csvShortOption) || (daoInfo.hasLongOpt() && daoInfo.getLongOpt().equals(csvLongOption)))
       return new CsvUserDao(daoInfo.getValue());
 
     //Parse ForgeRockDao
-    if(daoInfo.getOpt().equals(forgeRockShortOption) || (daoInfo.hasLongOpt() && daoInfo.getLongOpt().equals(forgeRockLongOption)))
-      return new ForgeRockUserDao(new OpenIdConfig(daoInfo.getValue(0),daoInfo.getValue(1),daoInfo.getValue(2)));
+    if (daoInfo.getOpt().equals(forgeRockShortOption)
+        || (daoInfo.hasLongOpt() && daoInfo.getLongOpt().equals(forgeRockLongOption)))
+      return new ForgeRockUserDao(new OpenIdConfig(daoInfo.getValue(0), daoInfo.getValue(1), daoInfo.getValue(2)));
 
     return new CsvUserDao("");
   }
-
 
   public static void main(String[] args) {
 
@@ -42,22 +42,11 @@ public class UserSyncApp {
     Options options = new Options();
 
     //Creating options
-    Option forgeRock = Option.builder(forgeRockShortOption)
-            .hasArg(true)
-            .argName("server addr> <username> <password")
-            .longOpt(forgeRockLongOption)
-            .desc("Option to specify forgeRock server")
-            .numberOfArgs(3)
-            .valueSeparator(' ')
-            .build();
-    Option csv = Option.builder(csvShortOption)
-            .hasArg(true)
-            .argName("file")
-            .longOpt(csvLongOption)
-            .desc("Option to Specify CSV file")
-            .numberOfArgs(1)
-            .valueSeparator(' ')
-            .build();
+    Option forgeRock = Option.builder(forgeRockShortOption).hasArg(true).argName("server addr> <username> <password")
+        .longOpt(forgeRockLongOption).desc("Option to specify forgeRock server").numberOfArgs(3).valueSeparator(' ')
+        .build();
+    Option csv = Option.builder(csvShortOption).hasArg(true).argName("file").longOpt(csvLongOption)
+        .desc("Option to Specify CSV file").numberOfArgs(1).valueSeparator(' ').build();
 
     //Add options to group
     options.addOption(forgeRock);
@@ -77,7 +66,7 @@ public class UserSyncApp {
       Option parsedOptions[] = cmd.getOptions();
 
       //check number of option
-      if(parsedOptions.length == 2){
+      if (parsedOptions.length == 2) {
         //Parse first option
         src = parseDaoInfo(parsedOptions[0]);
         //Parse second option
@@ -89,10 +78,9 @@ public class UserSyncApp {
       }
       printHelp(options);
       throw new RuntimeException("Invalid number of options");
-    }
-    catch (ParseException e){
+    } catch (ParseException e) {
       printHelp(options);
-      throw new RuntimeException("Invalid Arguments",e);
+      throw new RuntimeException("Invalid Arguments", e);
     }
 
   }
