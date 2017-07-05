3692fc08716690512c80e9c8e7f215ae1f6a6e3d
raisercostin <raisercostin@gmail.com>
2017-07-05 16:04:05 +0300

    standard format

--- src/test/java/ro/dcsi/internship/IntegrationTestConfig.java
+++ src/test/java/ro/dcsi/internship/IntegrationTestConfig.java
@@ -1,6 +1,5 @@
 package ro.dcsi.internship;
 
 public class IntegrationTestConfig {
-  public static OpenIdConfig testInstance = new OpenIdConfig("http://dcs-xps:8080", "openidm-admin",
-      "openidm-admin");
+  public static OpenIdConfig testInstance = new OpenIdConfig("http://dcs-xps:8080", "openidm-admin", "openidm-admin");
 }
