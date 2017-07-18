9a91d68393bf4f96882fc2e9ac1592374d28fe6a
sorindragan <sorin.dragan27@gmail.com>
2017-07-18 20:57:01 +0300

    Code that read and write users from csv files

--- usersync/src/main/java/ro/dcsi/internship/App.java
+++ usersync/src/main/java/ro/dcsi/internship/App.java
@@ -1,13 +1,49 @@
 package ro.dcsi.internship;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!!" );
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class ReadingUsers {
+	public void read() throws FileNotFoundException {
+		System.out.println("Reading users in order from a CSV file");
+        Scanner sc = new Scanner(new File("src/test/files/users.csv"));
+        sc.useDelimiter(",");
+        System.out.println("User list: ");
+        while (sc.hasNext()) {
+        	System.out.println(sc.next());
+        }
+        sc.close();
+	}
+}
+
+class WritingUsers {
+	public void write() throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter(new File("src/test/files/users2.csv"));
+		StringBuilder sb = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			String x = sc.next();
+			if(x.equals("end")) {
+				break;
+			} else {
+				sb.append(x);
+				sb.append(',');
+			}
+		}
+		sc.close();
+		pw.write(sb.toString());
+		pw.close();
+		System.out.println("Writing done!");
+	}
+}
+
+//this app deal only with users
+//TODO: add pass and other info..
+public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+        new ReadingUsers().read();
+        new WritingUsers().write();
     }
 }
