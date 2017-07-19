db2dbb79bd47668d459a0da81244cf2b83a3106e
raisercostin <raisercostin@gmail.com>
2017-07-19 17:47:53 +0300

    upgrade to junit-4.12 from 3.8.x

--- src/test/java/ro/dcsi/internship/AppTest.java
+++ src/test/java/ro/dcsi/internship/AppTest.java
@@ -1,37 +1,12 @@
 package ro.dcsi.internship;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 import java.io.FileNotFoundException;
 
-import junit.framework.Assert;
+import org.junit.Assert;
+import org.junit.Test;
 
-/**
- * Unit test for App.
- */
-public class AppTest extends TestCase {
-  /**
-   * Create the test case
-   *
-   * @param testName name of the test case
-   */
-  public AppTest(String testName) {
-    super(testName);
-  }
-
-  /**
-   * @return the suite of tests being tested
-   */
-  public static Test suite() {
-    return new TestSuite(AppTest.class);
-  }
-
-  /**
-   * My tests
-   * @throws FileNotFoundException 
-   */
+public class AppTest {
+  @Test
   public void testApp() throws FileNotFoundException {
     String testR = new ReadingUsers().read();
     String testW = new WritingUsers().verification();
