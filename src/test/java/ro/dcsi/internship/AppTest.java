49c527e96daef07022662dc30876ea6efe122511
iacobiulian <iacob.m96@gmail.com>
2017-07-21 14:09:03 +0300

    deleted what is not neeeded

--- src/test/java/ro/dcsi/internship/AppTest.java
+++ src/test/java/ro/dcsi/internship/AppTest.java
@@ -9,27 +9,10 @@ import org.junit.Test;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
-public class AppTest {
+public class AppTest
+{
   @Test
   public void testApp(){
-    App.main(null);
-  }
-  @Test
-  public void testApp2() throws FileNotFoundException {
-    String testR = new ReadingUsers().read();
-    String testW = new WritingUsers().verification();
-    //knowing the last user i want to know if the method      //read them all
-    //TODO review
-    Assert.assertEquals("ca\n", testR);
-    Assert.assertEquals("Writing done!", testW);
-  }
-  @Test
-  public void testApp3(){
-    App3.writeDataInFile("");
-    App3.readDataFromFile();
-  }
-  @Test
-  public void testApp4(){
     UserDao app= new UserDaoCostin();
     app.writeUsers("file1",new TheUser("ion"),new TheUser("gigi"));
     app.writeUsers("file2",new TheUser("costin"));
