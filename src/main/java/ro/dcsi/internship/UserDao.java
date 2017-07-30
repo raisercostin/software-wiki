0e9870e1f8e0f654da8b4d06f3b227a9b647f282
sorindragan <sorin.dragan27@gmail.com>
2017-07-30 23:48:32 +0300

    old vers

--- src/main/java/ro/dcsi/internship/UserDao.java
+++
@@ -1,9 +0,0 @@
-package ro.dcsi.internship;
-
-import java.util.List;
-
-public interface UserDao {
-  void writeUsers(String fileName, TheUser... users);
-
-  List<TheUser> readUsers(String fileName);
-}
