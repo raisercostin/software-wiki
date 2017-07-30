53886ecd78c594b2614c0ee15e9b4db2d8979d3c
sorindragan <sorin.dragan27@gmail.com>
2017-07-30 23:54:33 +0300

    redo stuff

---
+++ src/main/java/ro/dcsi/internship/UserDao.java
@@ -0,0 +1,9 @@
+package ro.dcsi.internship;
+
+import java.util.List;
+
+public interface UserDao {
+  void writeUsers(String fileName, TheUser... users);
+
+  List<TheUser> readUsers(String fileName);
+}
