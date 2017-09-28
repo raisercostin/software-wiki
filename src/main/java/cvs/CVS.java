076fe5f95eca73d86e3b61e8f7d67adffe96c95a
Gabriel Agapie <gabriel.agapie@gmail.com>
2017-09-28 13:30:11 +0300

    1:30 PM

--- src/main/java/cvs/CVS.java
+++ src/main/java/cvs/CVS.java
@@ -44,7 +44,11 @@ public class CVS
     public static void scrieCVS(String cale){
     	try(PrintStream out = new PrintStream(cale)){
     		for(int i =0;i<listaUseri.size();i++){
-    			out.print(listaUseri.get(i) + ",");
+    			User user =listaUseri.get(i);
+    			out.print(user.getNume() + ","
+    					+user.getPrenume()+ ","
+    					+user.getEmail() +"\n"
+    					);
     		}
     		
     	} catch (FileNotFoundException e) {
