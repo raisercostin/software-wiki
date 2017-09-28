372b16c8a239fbf5eb95803ac96e69f30a4a3391
VVasile3721 <vasile.v@yahoo.com>
2017-09-28 18:17:26 +0300

    tests in remote. pfuuuu

---
+++ src/main/java/writefile.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class writefile {
+	
+	private Formatter x;
+	
+	public void openFile(){		
+	try {
+		x = new Formatter("users2.csv");
+	}	
+	catch(Exception e) {
+	throw new RuntimeException(e);
+	}
+	}
+	
+	public void addUsers() {
+		x.format("%s, %s, %s", "Stefan Mardare", "34", "marrstef");
+	}
+	
+	public void closeFile() {
+		x.close();
+	}
+
+}
