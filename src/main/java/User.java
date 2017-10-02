05040943e6eb492eeace0e0beb6f0b1bdedce47f
VVasile3721 <vasile.v@yahoo.com>
2017-10-02 22:40:07 +0300

    feature added for diff columns

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,42 +1,19 @@
 
 public class User {
-	private String name;
-	private int age;
-	private String username;
-	
-	public User(String name, int age, String username) {
-		this.name = name;
-		this.age = age;
-		this.username = username;
+	private String nume;
+	private String prenume;
+	private String email;
+
+	public User(String nume, String prenume, String email) {
+		this.nume = nume;
+		this.prenume = prenume;
+		this.email = email;
 	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getAge() {
-		return age;
-	}
-	
-	public void setAge() {
-		this.age = age;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public void setUsername() {
-		this.username = username;
-	}
-	
+
 	@Override
 	public String toString() {
-		return "User: nume= " + name +", varsta= " + age + ", username= " + username;
+		return "User [nume=" + nume + ", prenume=" + prenume + ", email=" + email + "]";
 	}
+
 
 }
