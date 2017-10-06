e657ef8e60d8e528d7fe6c8720ecbfa0e9b40425
VVasile3721 <vasile.v@yahoo.com>
2017-10-06 19:05:42 +0300

    nothing much

--- src/main/java/ro/dsci/internship/UnirestForgeRockUserDao.java
+++ src/main/java/ro/dsci/internship/UnirestForgeRockUserDao.java
@@ -13,37 +13,40 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class UnirestForgeRockUserDao implements UserDao {
 
-  @Override
-  public List<User> readUsers(String locatie) {
-    try {
-      HttpResponse<JsonNode> getResponse = Unirest
-          .get("http://localhost:8080/openidm/managed/user?_prettyPrint=true&_queryId=query-all")
-          .header("Accept", "application/json").header("Content-Type", "application/json")
-          .header("X-Requested-With", "Swagger-UI").header("X-OpenIDM-Username", "openidm-admin")
-          .header("X-OpenIDM-Password", "openidm-admin").asJson();
+	@Override
+	public List<User> readUsers(String locatie) {
+		try {
+			HttpResponse<JsonNode> getResponse = Unirest
+					.get("http://localhost:8080/openidm/managed/user?_prettyPrint=true&_queryId=query-all")
+					.header("Accept", "application/json")
+					.header("Content-Type", "application/json")
+					.header("X-Requested-With", "Swagger-UI")
+					.header("X-OpenIDM-Username", "openidm-admin")
+					.header("X-OpenIDM-Password", "openidm-admin")
+					.asJson();
 
-      JSONObject body = getResponse.getBody().getObject();
-      System.out.println(body.toString(4));
-      JSONArray users = body.getJSONArray("result");
-      List<User> result = new ArrayList<>();
-      for (int i = 0, maxi = users.length(); i < maxi; i++) {
-        result.add(toUser((JSONObject) users.get(i)));
-      }
-      return result;
-    } catch (UnirestException e) {
-      throw new RuntimeException("Wrapped checked exception.", e);
-    }
-  }
+			JSONObject body = getResponse.getBody().getObject();
+			System.out.println(body.toString(4));
+			JSONArray users = body.getJSONArray("result");
+			List<User> result = new ArrayList<>();
+			for (int i = 0, maxi = users.length(); i < maxi; i++) {
+				result.add(toUser((JSONObject) users.get(i)));
+			}
+			return result;
+		} catch (UnirestException e) {
+			throw new RuntimeException("Wrapped checked exception.", e);
+		}
+	}
 
-  private User toUser(JSONObject object) {
-    return new User(object.getString("userName"), object.getString("givenName"), object.getString("sn"),
-        object.getString("mail"));
-  }
+	private User toUser(JSONObject object) {
+		return new User(object.getString("userName"), object.getString("givenName"), object.getString("sn"),
+				object.getString("mail"));
+	}
 
-  @Override
-  
-  public void writeUsers(List<User> users, String locatie) {
-    // TODO Auto-generated method stub
-    throw new RuntimeException("Not implemented yet!!!");
-  }
+	@Override
+
+	public void writeUsers(List<User> users, String locatie) {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented yet!!!");
+	}
 }
