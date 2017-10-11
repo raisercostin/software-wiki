691865cad9dc809a1e08835de4afbf76420846ee
VVasile3721 <vasile.v@yahoo.com>
2017-10-11 22:32:09 +0300

    copy from file to file

--- src/main/java/ro/dsci/internship/UserSyncApp.java
+++ src/main/java/ro/dsci/internship/UserSyncApp.java
@@ -1,7 +1,30 @@
 package ro.dsci.internship;
 
+import java.util.List;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 public class UserSyncApp {
+
+  @Parameter(names = { "--csv", "-r" })
+  private static String readFromFile;
+
+  @Parameter(names = { "--csvWrite", "-w" })
+  private static String writeToFile;
+
   public static void main(String... args) {
-    throw new RuntimeException("Not implemented yet"); 
+
+    UserSyncApp userSyncApp = new UserSyncApp();
+    
+    JCommander.newBuilder()
+    .addObject(userSyncApp)
+    .build()
+    .parse(args);
+
+    VladUserDao userDao = new VladUserDao();
+    List<User> users = userDao.readUsers(readFromFile);
+    userDao.writeUsers(users, writeToFile);
+
   }
 }
