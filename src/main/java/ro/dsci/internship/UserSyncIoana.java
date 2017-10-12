68fc0186d5160659c1bd0db4cff31072565f2448
IoanaGabriela <gabriela.ioana.21@gmail.com>
2017-10-12 16:05:49 +0300

    IoanaUserSync

---
+++ src/main/java/ro/dsci/internship/UserSyncIoana.java
@@ -0,0 +1,13 @@
+package ro.dsci.internship;
+
+import java.util.List;
+
+public class UserSyncIoana 
+	{
+
+  static UnirestForgeRockUserDao adminDao = new UnirestForgeRockUserDao();
+  
+  
+ 
+
+	}
