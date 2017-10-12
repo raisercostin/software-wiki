9f935eda43c813515f87c9d5a6ba8cc4f231ffd8
IoanaGabriela <gabriela.ioana.21@gmail.com>
2017-10-12 17:31:21 +0300

    try

--- src/main/java/ro/dsci/internship/UserSyncIoana.java
+++ src/main/java/ro/dsci/internship/UserSyncIoana.java
@@ -2,12 +2,29 @@ package ro.dsci.internship;
 
 import java.util.List;
 
-public class UserSyncIoana 
-	{
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.internal.Sets;
 
-  static UnirestForgeRockUserDao adminDao = new UnirestForgeRockUserDao();
-  
-  
- 
+public class UserSyncIoana {
 
-	}
+  @Parameter(names = { "-csv", "-r" }, description = "to read from csv")
+  private static String readCsv;
+
+  @Parameter(names = { "-csvWrite", "-w" }, description = "to write to csv")
+  private static String writeCsv;
+
+  @Parameter(names = { "-forgerock" }, description = "forgerock server")
+  private static String server;
+
+  @Parameter(names = { "-user" }, description = "password field")
+  private static String password;
+
+  public static void main(String[] args) {
+
+    Sets settings = new Sets();
+    new JCommander(settings, args); // simple one-liner
+    // do the logic
+
+  }
+}
